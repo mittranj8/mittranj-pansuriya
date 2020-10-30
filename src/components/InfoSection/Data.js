096fd23d609efcc191a7c9306492b4b0a8117790
Mittranj <mpansuriya@unomaha.edu>
2020-10-30 12:36:17 -0500

    added row data to info section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,53 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Something About Me",
+  headLine: "I am a UI Developer and a Learner!",
+  description:
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed provident officiis facilis libero quae. Perspiciatis quia voluptates eum dolor magnam voluptatibus reprehenderit nostrum, quas doloremque cumque eveniet laborum commodi repellendus.",
+  imgStart: false,
+  buttonLabel: "My Skills",
+  img: require("../../assets/images/aboutMe.svg").default,
+  alt: "About Image",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "projects",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Here is some of my projects...",
+  headLine: "These are some of my recent works!!",
+  description:
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed provident officiis facilis libero quae. Perspiciatis quia voluptates eum dolor magnam voluptatibus reprehenderit nostrum, quas doloremque cumque eveniet laborum commodi repellendus.",
+  imgStart: true,
+  buttonLabel: "My Skills",
+  img: require("../../assets/images/myInfo.svg").default,
+  alt: "Project Image",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "work-experience",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Here is my work experience",
+  headLine: "Checkout my work history!",
+  description:
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed provident officiis facilis libero quae. Perspiciatis quia voluptates eum dolor magnam voluptatibus reprehenderit nostrum, quas doloremque cumque eveniet laborum commodi repellendus.",
+  imgStart: false,
+  buttonLabel: "My Experience",
+  img: require("../../assets/images/myExperience.svg").default,
+  alt: "Experience Image",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
