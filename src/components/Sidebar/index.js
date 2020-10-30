c8919237c4e1ce3a7048275a5029df45546aaee4
Mittranj <mpansuriya@unomaha.edu>
2020-10-29 19:38:03 -0500

    added sidebar component

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  CloseIcon,
+  Icon,
+  SidebarContainer,
+  SidebarLink,
+  SidebarMenu,
+  SidebarRoute,
+  SidebarWrapper,
+  SideBtnWrap,
+} from "./SidebarElements";
+
+const Sidebar = ({ isOpen, toggle }) => {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about" onClick={toggle}>
+            About
+          </SidebarLink>
+          <SidebarLink to="projects" onClick={toggle}>
+            Projects
+          </SidebarLink>
+          <SidebarLink to="work-experience" onClick={toggle}>
+            Work Experience
+          </SidebarLink>
+          <SidebarLink to="contact" onClick={toggle}>
+            Contact
+          </SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to="/download">Download Resume</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
