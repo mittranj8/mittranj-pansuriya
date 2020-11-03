8ed5a9ac9667d353d9bc4734bdf6884b4c397d9b
Mittranj <mpansuriya@unomaha.edu>
2020-11-03 15:33:49 -0600

    added routing to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages";
+import ResumePage from "./pages/resumePage";
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/resume" component={ResumePage} exact />
+      </Switch>
     </Router>
   );
 }
