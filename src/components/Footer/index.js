820962e9f8263d5ee3214fa56d8151477a272074
Mittranj <mpansuriya@unomaha.edu>
2020-11-03 15:34:07 -0600

    added footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,100 @@
+import { Facebook, GitHub, Instagram, LinkedIn } from "@material-ui/icons";
+import React from "react";
+import {
+  FooterContainer,
+  FooterLink,
+  FooterLinkItems,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkTitle,
+  FooterWrap,
+  SocialIconLink,
+  SocialIcons,
+  SocialLogo,
+  SocialMedia,
+  SocialMediaWrap,
+  WebsiteRights,
+} from "./FooterElements";
+import { animateScroll as scroll } from "react-scroll";
+
+const Footer = () => {
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
+
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/">Terms of Services</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/">Terms of Services</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/resume">Terms of Services</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/">Terms of Services</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/" onClick={toggleHome}>
+              Mittranj
+            </SocialLogo>
+            <WebsiteRights>
+              Made with ❤️ by Mittranj © {new Date().getFullYear()} All rights
+              reserved.
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink
+                href="//www.linkedin.com/in/mittranj-pansuriya/"
+                target="_blank"
+                aria-label="LinkedIn"
+              >
+                <LinkedIn />
+              </SocialIconLink>
+              <SocialIconLink
+                href="//www.github.com/mittranj8"
+                target="_blank"
+                aria-label="GitHub"
+              >
+                <GitHub />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                <Instagram />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                <Facebook />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
